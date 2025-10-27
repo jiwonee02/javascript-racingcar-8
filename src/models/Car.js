e431b61e911ce08cc8ada1fb6ef56e616ca6ec47
jiwonee02 <nepthun@naver.com>
2025-10-27 16:54:03 +0900

    feat(model): Car 모델 추가

---
+++ src/models/Car.js
@@ -0,0 +1,20 @@
+export default class Car {
+    #name;
+    #position;
+
+    constructor(name) {
+        this.#name = name;
+        this.#position = 0;
+    }
+
+    get name() {
+        return this.#name;
+    }
+    get position() {
+        return this.#position;
+    }
+
+    move() {
+        this.#position += 1;
+    }
+}
